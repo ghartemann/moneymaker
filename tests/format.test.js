95799aa527f3bdfa927583dbc3d8472f4256cdaf
ghartemann <ghartemann@com-company.fr>
2025-04-22 16:43:06 +0200

    save

--- tests/format.test.js
+++ tests/format.test.js
@@ -5,3 +5,8 @@ test('formatPrice', () => {
     expect(useFormat().formatPrice(123456789)).toBe('€123,456,789.00');
     expect(useFormat().formatPrice(123456789.123)).toBe('€123,456,789.12');
 });
+
+test('formatHours', () => {
+    expect(useFormat().formatHours(22)).toStrictEqual(['22 hours']);
+    expect(useFormat().formatHours(22, true)).toStrictEqual(['22 h']);
+});
